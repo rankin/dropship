efcd0e465a12eeb28e1d0f90f3b46514f4125e3f
Christopher Rankin <rankin.devon@gmail.com>
2012-09-13 00:33:31 -0500

    Spree store for drop shipper

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dropship::Application.config.secret_token = '86771e68de7dce6b48bb1d1bfa3a0efcd7e7f30161c9fb971686f44fb8350aff4b0718bd313a5944152b9f014f1577fed7d18fac444d5e8a533bd22f88e6ebef'
